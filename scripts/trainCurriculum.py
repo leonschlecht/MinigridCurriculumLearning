9b64e08463d8aef72f2423dc8133460b9090bafb
leonschlecht <73598045+leonschlecht@users.noreply.github.com>
2023-04-22 16:36:41 +0200

    temporary commit ; initial pymoo stub

--- scripts/trainCurriculum.py
+++ scripts/trainCurriculum.py
@@ -1,50 +1,83 @@
-from pymoo.core.result import Result
+from datetime import datetime
+
+import numpy as np
+from gymnasium.envs.registration import register
+from pymoo.algorithms.moo.nsga2 import NSGA2
+from pymoo.core.evaluator import Evaluator
+from pymoo.core.population import Population
+from pymoo.core.problem import Problem
 
 import utils
-from utils import ENV_NAMES
 from curricula import linear, adaptive, EvolutionaryCurriculum
-from datetime import datetime
-from gymnasium.envs.registration import register
-import numpy as np
-from pymoo.core.problem import Problem
-from pymoo.operators.crossover.pntx import TwoPointCrossover
-from pymoo.operators.mutation.bitflip import BitflipMutation
-from pymoo.operators.sampling.rnd import BinaryRandomSampling
-from pymoo.algorithms.moo.nsga2 import NSGA2
-from pymoo.problems import get_problem
-from pymoo.optimize import minimize
-from pymoo.visualization.scatter import Scatter
-
 from utils import ENV_NAMES
+
+
+def createFirstGeneration(curriculaList):
+    indices = []
+    for i in range(len(curriculaList)):
+        indices.append([])
+        for env in curriculaList[i]:
+            indices[i].append(ENV_NAMES.ALL_ENVS.index(env))
+    return indices
 
 
 def tryEvolStuff():
     objectives = 1
-    curric1 = [ENV_NAMES.DOORKEY_6x6, ENV_NAMES.DOORKEY_5x5, ENV_NAMES.DOORKEY_5x5]
-    curric2 = [ENV_NAMES.DOORKEY_8x8, ENV_NAMES.DOORKEY_16x16, ENV_NAMES.DOORKEY_16x16]
+    curric1 = [ENV_NAMES.DOORKEY_5x5, ENV_NAMES.DOORKEY_5x5, ENV_NAMES.DOORKEY_6x6, ENV_NAMES.DOORKEY_6x6]
+    curric2 = [ENV_NAMES.DOORKEY_8x8, ENV_NAMES.DOORKEY_16x16, ENV_NAMES.DOORKEY_16x16, ENV_NAMES.DOORKEY_16x16]
     xupper = len(ENV_NAMES.ALL_ENVS)
-    curriculaList = [curric1]
+    curriculaList = [curric2, curric1]
     inequalityConstr = 0
     curricProblem = CurriculumProblem(curriculaList, objectives, inequalityConstr, xupper)
+
+    X = createFirstGeneration(curriculaList)
+    pop = Population.new("X", X)
+    Evaluator().eval(curricProblem, pop)
 
     algorithm = NSGA2(pop_size=len(curriculaList),
                       # sampling=BinaryRandomSampling(),
                       # crossover=TwoPointCrossover(),
                       # mutation=BitflipMutation(),
-                      eliminate_duplicates=True
+                      eliminate_duplicates=True,
                       )
-
+    """
     res: Result = minimize(curricProblem,
                            algorithm,
-                           ('n_gen', 50),  # terminationCriterion
+                           ('n_gen', 300),  # terminationCriterion
                            seed=1,
-                           verbose=True)  # True = print output during training
-
+                           verbose=False,
+                           x0=curriculaList
+                           )  # True = print output during training
     # Scatter().add(res.F).show()
-    print(res.F)
-    print(res.X)
-    print(curricProblem.curricula)
-    print("We would append ", ENV_NAMES.ALL_ENVS[round(res.X[0])])
+    """
+
+    # prepare the algorithm to solve the specific problem (same arguments as for the minimize function)
+    algorithm.setup(curricProblem, termination=('n_gen', 300), seed=1, verbose=False)
+    initialPop = Population.new("X", X)
+    # Evaluator().eval(curricProblem, pop)
+    # algorithm.evaluator.eval(curricProblem, initialPop)
+    # algorithm.tell(infills=initialPop)
+
+    while algorithm.has_next():
+        # ask the algorithm for the next solution to be evaluated
+        if initialPop is not None:
+            pop = algorithm.ask()
+        else:
+            pop = initialPop
+            initialPop = None
+        # evaluate the individuals using the algorithm's evaluator (necessary to count evaluations for termination)
+        algorithm.evaluator.eval(curricProblem, pop)
+        # returned the evaluated individuals which have been evaluated or even modified
+
+        algorithm.tell(infills=pop)
+
+        # do same more things, printing, logging, storing or even modifying the algorithm object
+        # print("n_gen, n_eval:", algorithm.n_gen, algorithm.evaluator.n_eval)
+        print("----", "pop X:", np.round(pop.get("X")), pop.get("F"), "-----", sep="\n")
+        # TODO update the model with best individual
+    res = algorithm.result()
+    print("hash", res.F.sum())
+    print(np.round(res.X))
 
     return 0
 
@@ -61,16 +94,14 @@ def main():
 
     txtLogger = utils.get_txt_logger(utils.get_model_dir(args.model))
 
-    # TODO limit max frames per env in evaluation
     # TODO fix iterations (so it doesnt overshoot the amount; maybe calculate with exact frame nrs or use updates)
     startTime = datetime.now()
 
     ############
 
-    tryEvolStuff()
-    exit(999)
     if args.trainEvolutionary:
         e = EvolutionaryCurriculum(txtLogger, startTime, args)
+        tryEvolStuff()
     elif args.trainAdaptive:
         adaptive.adaptiveCurriculum(txtLogger, startTime, args)
 
@@ -79,6 +110,7 @@ def main():
 
 
 def registerEnvs():
+    # TODO find better way so that the max_steps decrease over time
     register(
         id="MiniGrid-Empty-Random-8x8-v0",  # todo use from ENV_NAMES
         entry_point="minigrid.envs:EmptyEnv",
@@ -118,15 +150,11 @@ def registerEnvs():
 
 class CurriculumProblem(Problem):
     def __init__(self, curricula: list, n_obj, n_ieq_constr, xu):
-        n_var = len(curricula)
-        # also wenn wir die Curricula (= je 1 individuum?), versuhcen wir die einzelnen listeneinträge element [xl, xu]
-        # deshalb sollte das ein array sein; bzw. ist wahrscheinlich egal wenn es für alle gleich ist
-        # Man könnte evtl mit ieq_constr ausprobieren, dass man homogene curricula vermeiden möchte oder so ?
-        # Erstmal mit n_var = 1 ausprobieren; evtl später noch siherstellen dass n_var isch wirklich aus den curricula ergibt
-        xl = np.zeros(n_var)
-        xu = np.full(n_var, xu, dtype=int)
-        print(xu)
-        assert xl.shape == xu.shape
+        assert len(curricula) > 0
+        n_var = len(curricula[0])
+        # maybe try to avoid homogenous curricula with ieq constraints (?)
+        xl = np.full(n_var, -0.49)
+        xu = np.full(n_var, xu - 0.51, dtype=float)
         super().__init__(n_var=n_var,  # the amount of curricula to use
                          n_obj=n_obj,  # maximizing the overall reward = 1 objective
                          n_ieq_constr=n_ieq_constr,  # 0 ?
@@ -136,32 +164,22 @@ class CurriculumProblem(Problem):
         self.N = 0
         # elementwise vs problem vs ?
 
-        # das __evaluate könnte simulieren, was für rewards bestimmte Curricula bringen
-        # Dwenn wir mehrere gens haben, wird das model währenddessen nicht (?) geupdated
-        # alternativ könnte es sein, dass man den code in die epoch loo
-
         """
-        epoch loop
-            führe den RH aus
-            führe Evolution aus, um zu bestimmten, wie die nächsten Curricula aussehen (input: aktueller stand des models,
-                die rewards der aktuellen curricula; 
-                die weiterführenden generationen würden dann den ganzen Trainingsprozess noch mal simulieren ???
-                Falls nur 1 Generation: dann würde er versuchen zu bestimmen, was basierend auf: die curricula hatten die Rewards, mein Ziel ist es das zu maximieren. Wie sollte es nächstes mal aussehen?
-            update das Model auf den snapshot fortschritt des besten Curriculums
+        (0) epoch loop
+        (1)     führe den RH aus wie ich es bisher implementiert habe
+        (2)     führe Evolution aus, um zu bestimmten, wie die nächsten Curricula aussehen (input: aktueller stand des models,
+                    die rewards der aktuellen curricula, ... ? 
+                    Was würde die _evaluate von pymoo denn aufrufen hier? Und was ist wenn Generationen > 1
+        (3) update das Model auf den snapshot fortschritt des besten Curriculums, wie in (1) bestimmt
         """
 
-
         """
-        start evolution
+        start evolution durch minimize(...)
         in der _evaluate:
-            bestimmte den Reward der curricula (also ruf die train methode auf)
+            bestimmte den Reward pro curriculum (also ruf die train methode für jedes curriculum auf)
             update das model
-            bestimmte die nächsten Curricula
-            
-            
-        Was optimiert die evolution hier überhaupt ???
+            bestimmte die nächsten Curricula            
         """
-
         # F: what we want to maximize: ---> pymoo minimizes, so it should be -reward
         # G:# Inequality constraint;
         # H is EQ constraint: maybe we can experiment with the length of each curriculum;
@@ -169,23 +187,35 @@ class CurriculumProblem(Problem):
 
     def _evaluate(self, x, out, *args, **kwargs):
         rewards = np.zeros(len(self.curricula))
-        for i in range(len(self.curricula)):
-            cur = self.curricula[i]
-            rounded = round(x[i][0])
-            # cur.append(ENV_NAMES.ALL_ENVS[rounded])
-             # self.trainCurriculum(cur)
-            rewards[i] = -200
-        print(rewards)
+        # x should be of size len(curric) x len(curric[i]); so if we have 5 envs x 4 curric, it should be 5x4.
+        # right now it is len(curric) x len(curric); every row represents one solution for the problem
+        curricula = self.evolXToCurriculum(x)
+
+        # TODO call the actual train method
+        """
+        for i in range(len(curricula)):
+            cur = curricula[i]
+            reward = 0
+            for env in cur:
+                if env == ENV_NAMES.DOORKEY_16x16:
+                    reward += 10
+            rewards[i] = reward
+        """
         out["F"] = -rewards
         self.N += 1
 
-    def trainCurriculum(self, cur):
-        # this should be at the end of the evolutionary.py file in the train loop ? and it should be called ONCE per curricula I think
-        print(1)
-        return self.N
+    @staticmethod
+    def evolXToCurriculum(x):
+        result = []
+        for i in range(x.shape[0]):
+            result.append([])
+            curric = x[i]
+            for j in range(curric.shape[0]):
+                rounded = round(x[i][j])
+                result[i].append(ENV_NAMES.ALL_ENVS[rounded])
+        return result
 
 
 if __name__ == "__main__":
     registerEnvs()
     main()
-    N = 0
