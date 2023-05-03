implement correct time needed calculation for log; evaluation env depending on performance registering

curricula/RollingHorizonEvolutionaryAlgorithm.py
@@ -29,6 +29,7 @@ class RollingHorizonEvolutionaryAlgorithm:
         self.numCurric = args.numCurric
         self.envsPerCurric = args.envsPerCurric
         self.cmdLineString = args.cmdLineString
+        self.lastEpochStartTime = startTime
 
         # Pymoo parameters
         objectives = 1
@@ -39,11 +40,11 @@ class RollingHorizonEvolutionaryAlgorithm:
         self.ITERATIONS_PER_ENV = args.iterPerEnv
         self.iterationsDone = 0
         self.txtLogger = txtLogger
-        self.startTime = startTime
         self.selectedModel = utils.getEpochModelName(args.model, 0)  # TODO is this useful for 0?
         self.trainingEpochs = args.trainEpochs
         self.nGen = args.nGen
         self.trainEpochs = args.trainEpochs
+        self.trainingTime = 0
 
         self.trainingInfoJson = {}
         self.logFilePath = os.getcwd() + "\\storage\\" + args.model + "\\status.json"  # TODO maybe outsource
@@ -68,9 +69,10 @@ class RollingHorizonEvolutionaryAlgorithm:
             iterationsDone = train.main(iterationsDone + self.ITERATIONS_PER_ENV, iterationsDone, nameOfCurriculumI,
                                         self.curricula[i][j], self.args, self.txtLogger)
             reward += ((self.gamma ** j) * evaluate.evaluateAgent(nameOfCurriculumI, self.args))  # TODO or (j+1) ?
+            # 0 <= reward <= len(curricula[i]) --> TODO find good values
             self.txtLogger.info(f"\tIterations Done {iterationsDone}")
             if j == 0:
-                self.ITERATIONS_PER_ENV = iterationsDone + 1 # TODO test this ;
+                self.ITERATIONS_PER_ENV = iterationsDone + 1  # TODO test this ;
                 utils.copyAgent(src=nameOfCurriculumI, dest=utils.getModelWithCandidatePrefix(
                     nameOfCurriculumI))  # save TEST_e1_curric0 -> + _CANDIDATE
             self.txtLogger.info(f"Trained iteration j={j} of curriculum {nameOfCurriculumI} ")
@@ -87,7 +89,7 @@ class RollingHorizonEvolutionaryAlgorithm:
         self.txtLogger.info(f"Rewards: {self.trainingInfoJson[rewardsKey]}")
 
         now = datetime.now()
-        timeDiff = (now - self.startTime).total_seconds()
+        timeDiff = 0
         print(timeDiff)
         self.txtLogger.info(f"Time ended at {now} , total training time: {timeDiff}")
         self.txtLogger.info("-------------------\n\n")
@@ -117,7 +119,9 @@ class RollingHorizonEvolutionaryAlgorithm:
                                  rewardsKey: {},
                                  actualPerformance: [],
                                  epochsDone: 1,
-                                 startTime: self.startTime,
+                                 startTime: self.lastEpochStartTime,
+                                 epochTrainingTime: [],
+                                 sumTrainingTime: 0,
                                  cmdLineString: self.cmdLineString,
                                  numFrames: 0}
         self.saveJsonFile(self.logFilePath, self.trainingInfoJson)
@@ -128,6 +132,14 @@ class RollingHorizonEvolutionaryAlgorithm:
             f.write(json.dumps(jsonBody, indent=4, default=str))
 
     def updateTrainingInfo(self, epoch: int, bestCurriculum: list, currentRewards, currentScore: float, popX) -> None:
+        """
+        Updates the training info dictionary
+        :param epoch: current epoch
+        :param bestCurriculum: the curriculum that had the highest reward in the latest epoch
+        :param currentRewards: the dict of rewards for each generation and each curriculum
+        :param currentScore: the current best score
+        :param popX: the pymoo X parameter for debugging purposes
+        """
         self.trainingInfoJson[epochsDone] = epoch + 1
         self.trainingInfoJson[numFrames] = self.iterationsDone
 
@@ -137,6 +149,13 @@ class RollingHorizonEvolutionaryAlgorithm:
         self.trainingInfoJson[actualPerformance].append([currentScore, bestCurriculum])
         self.trainingInfoJson[curriculaEnvDetails]["epoch" + str(epoch)] = self.curriculaEnvDetails
 
+        now = datetime.now()
+        timeSinceLastEpoch = (now - self.lastEpochStartTime).total_seconds()
+        self.trainingInfoJson[epochTrainingTime].append(timeSinceLastEpoch)
+        self.trainingInfoJson[sumTrainingTime] += timeSinceLastEpoch
+        self.lastEpochStartTime = now
+
+        # Debug Logs
         self.trainingInfoJson["currentListOfCurricula"] = self.curricula  # TODO is this useful?
         self.trainingInfoJson["curriculumListAsX"] = popX
 
@@ -250,8 +269,7 @@ class RollingHorizonEvolutionaryAlgorithm:
             self.iterationsDone = self.trainingInfoJson[numFrames]
             startEpoch = self.trainingInfoJson[epochsDone]
             rewardsDict = self.trainingInfoJson[rewardsKey]
-            self.startTime = datetime.fromisoformat(self.trainingInfoJson[startTime])
-            # TODO calculate endTime during each save, and then claculate total training time
+
             # delete existing folders, that were created ---> maybe just last one because others should be finished ...
             for k in range(self.numCurric):
                 path = utils.getModelWithCurricSuffix(self.model, startEpoch, k)
@@ -290,10 +308,11 @@ class RollingHorizonEvolutionaryAlgorithm:
         :return: the rewards after the rolling horizon
         """
         curricula = self.evolXToCurriculum(evolX)
-        self.curricula = curricula # TODO this is probably useless; maybe give it as param or store it otherwise. Im not sure how much other methods rely on this though
+        self.curricula = curricula  # TODO this is probably useless; maybe give it as param or store it otherwise. Im not sure how much other methods rely on this though
         rewards = np.zeros(len(curricula))
         for i in range(len(curricula)):
-            rewardI = self.trainEachCurriculum(i, self.iterationsDone, genNr) # TODO. test if this actually uses correct curricula !!
+            rewardI = self.trainEachCurriculum(i, self.iterationsDone,
+                                               genNr)  # TODO. test if this actually uses correct curricula !!
             rewards[i] = rewardI
         self.currentRewards[GEN_PREFIX + str(genNr)] = rewards
         self.curriculaEnvDetails[GEN_PREFIX + str(genNr)] = curricula
@@ -371,3 +390,5 @@ epochsDone = "epochsDone"
 startTime = "startTime"
 numFrames = "numFrames"
 cmdLineString = "cmdLineString"
+epochTrainingTime = "epochTrainingTime"
+sumTrainingTime = "sumTrainingTime"

scripts/trainCurriculum.py
@@ -1,10 +1,13 @@
 import sys
 from datetime import datetime
+
+import numpy as np
 from gymnasium.envs.registration import register
 
 import utils
 from curricula import linearCurriculum, RollingHorizonEvolutionaryAlgorithm, \
     BiasedRandomRollingHorizon, adaptiveCurriculum
+from utils import ENV_NAMES
 
 
 def main():
@@ -35,42 +38,52 @@ def main():
 
 
 def registerEnvs():
-    # TODO find better way so that the max_steps decrease over time
+    """
+    Registers the envs before the training. Each env has 3 difficulty settings, whereby they decrease their maxsteps
+    """
+    """
     register(
-        id="MiniGrid-Empty-Random-8x8-v0",  # todo use from ENV_NAMES
+        id="MiniGrid-Empty-Random-8x8-v0",
         entry_point="minigrid.envs:EmptyEnv",
         kwargs={"size": 8, "agent_start_pos": None},
-    )
+    ) """
 
-    register(
-        id="MiniGrid-Empty-Random-16x16-v0",
-        entry_point="minigrid.envs:EmptyEnv",
-        kwargs={"size": 16, "agent_start_pos": None},
-    )
+    maxSteps16x16 = 16 ** 2 * 10
+    maxSteps8x8 = 8 ** 2 * 10
+    maxSteps6x6 = 6 ** 2 * 10
+    maxSteps5x5 = 5 ** 2 * 10
+    maxSteps = np.array([maxSteps16x16, maxSteps8x8, maxSteps6x6,maxSteps5x5])
+    difficulty = np.array([1, 0.33, 0.11])
+    result = np.matmul(maxSteps.reshape(-1, 1), difficulty.reshape(1, -1))
+    print(result)
 
-    register(
-        id="MiniGrid-DoorKey-16x16-custom",
-        entry_point="minigrid.envs:DoorKeyEnv",
-        kwargs={"size": 16, "max_steps": 300},
-    )
+    for i in range(len(difficulty)):
+        register(
+            id=ENV_NAMES.DOORKEY_16x16 + "-custom" + str(i),
+            entry_point="minigrid.envs:DoorKeyEnv",
+            kwargs={"size": 16, "max_steps": result[3][i]},
+        )
 
-    register(
-        id="MiniGrid-DoorKey-8x8-custom",
-        entry_point="minigrid.envs:DoorKeyEnv",
-        kwargs={"size": 8, "max_steps": 200},
-    )
+        register(
+            id=ENV_NAMES.DOORKEY_8x8 + "-custom" + str(i),
+            entry_point="minigrid.envs:DoorKeyEnv",
+            kwargs={"size": 8, "max_steps": result[2][i]},
+        )
 
-    register(
-        id="MiniGrid-DoorKey-6x6-custom",
-        entry_point="minigrid.envs:DoorKeyEnv",
-        kwargs={"size": 6, "max_steps": 125},
-    )
+        register(
+            id=ENV_NAMES.DOORKEY_6x6 + "-custom" + str(i),
+            entry_point="minigrid.envs:DoorKeyEnv",
+            kwargs={"size": 6, "max_steps": result[1][i]},
+        )
 
-    register(
-        id="MiniGrid-DoorKey-5x5-custom",
-        entry_point="minigrid.envs:DoorKeyEnv",
-        kwargs={"size": 5, "max_steps": 100},
-    )
+        register(
+            id=ENV_NAMES.DOORKEY_5x5 + "-custom" + str(i),
+            entry_point="minigrid.envs:DoorKeyEnv",
+            kwargs={"size": 5, "max_steps": result[0][i]},
+        )
+
+    # TODO vllt ist register gar nicht so teuer/umständlich, so dass man das einfach exakt je nach performance berechnen kann
+
 
 
 if __name__ == "__main__":