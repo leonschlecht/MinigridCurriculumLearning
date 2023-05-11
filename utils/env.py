fb87652f8bfa777e03b316385816813b49bf79b0
leonschlecht <73598045+leonschlecht@users.noreply.github.com>
2023-05-11 17:53:58 +0200

    apply git stash

--- utils/env.py
+++ utils/env.py
@@ -6,4 +6,12 @@ def make_env(env_key, seed=None, render_mode=None):
     env.reset(seed=seed)
     # env.observation_space = env_obs
     return ViewSizeWrapper(env, agent_view_size=5)
+    """
+    obs, _ = env.reset()
+    print(obs["image"].shape)
+    env_obs = ViewSizeWrapper(env, agent_view_size=5)
+    obs, _ = env_obs.reset()
+    print(obs["image"].shape)
+    print("--------------------------------")
+    """
     return env
