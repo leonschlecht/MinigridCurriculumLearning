0b1c9c558ffb3041376627e59f6f4b77ee000265
leonschlecht <73598045+leonschlecht@users.noreply.github.com>
2023-05-11 17:53:10 +0200

    create viewsize branch

--- utils/env.py
+++ utils/env.py
@@ -4,4 +4,6 @@ import gymnasium as gym
 def make_env(env_key, seed=None, render_mode=None):
     env = gym.make(env_key, render_mode=render_mode)
     env.reset(seed=seed)
+    # env.observation_space = env_obs
+    return ViewSizeWrapper(env, agent_view_size=5)
     return env
