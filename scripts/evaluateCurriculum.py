7c946a0760cf9153b713e126089393e390605337
leonschlecht <73598045+leonschlecht@users.noreply.github.com>
2023-06-02 17:01:08 +0200

    env distribution plot refactoring and multi variant idea

--- scripts/evaluateCurriculum.py
+++ scripts/evaluateCurriculum.py
@@ -2,10 +2,13 @@ import os
 from pathlib import Path
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 from scripts.Result import Result
 from utils import storage
 from utils.curriculumHelper import *
+from matplotlib.ticker import MaxNLocator
+
 
 
 def plotPerformance(allYValues: list[list[float]], allXValues: list[list[int]], maxReward: int, title: str, modelNames: list[str]):
@@ -61,7 +64,7 @@ def plotDistributionOfBestCurric(resultClassesList: list[Result], titleInfo: str
 
 def plotSnapshotEnvDistribution(resultClassesList: list[Result], titleInfo: str, modelNamesList: list):
     snapshotDistributions = [res.snapshotEnvDistribution for res in resultClassesList]
-    plotEnvsUsedDistrSubplot(snapshotDistributions, titleInfo, modelNamesList)
+    plotEnvsUsedDistribution(snapshotDistributions, titleInfo, modelNamesList)
 
 
 # TODO allCurricDsitribution
@@ -88,9 +91,7 @@ def plotEnvsUsedDistrSubplot(allEnvDistributions: list[dict], titleInfo: str, mo
         envOccurrences = list(finalDict.values())
 
         bar_container = axes[envDistIndex].bar(envs, envOccurrences, label=modelNamesList[envDistIndex])
-
-        # Add labels to the bars
-        axes[envDistIndex].bar_label(bar_container, labels=envOccurrences, fontsize=12, padding=5)
+        # axes[envDistIndex].bar_label(bar_container, labels=envOccurrences, fontsize=12, padding=5)
 
         axes[envDistIndex].set_ylabel('Occurrence')
         axes[envDistIndex].set_title(titleInfo)
@@ -101,46 +102,56 @@ def plotEnvsUsedDistrSubplot(allEnvDistributions: list[dict], titleInfo: str, mo
 
 
 def plotEnvsUsedDistribution(allEnvDistributions: list[dict], titleInfo: str, modelNamesList):
-    # extract the numeric part of the keys of the envDistribution
-    print(allEnvDistributions)
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(10, 8))
+    num_distributions = len(allEnvDistributions)
+    bar_width = 0.5 / num_distributions
 
-    for i in range(len(allEnvDistributions)):
-        envDistribution = allEnvDistributions[i]
-        numericStrDict = {numKey.split('-')[-1]: val for numKey, val in envDistribution.items()}
-        keyValues = sorted([int(fullKey.split("x")[0]) for fullKey in numericStrDict])
+    print(allEnvDistributions)
+    allEnvs = []
+    for envDistr in allEnvDistributions:
+        for key in envDistr.keys():
+            if key not in allEnvs:
+                allEnvs.append(key)
+    x_offset = -bar_width * (num_distributions - 1) / 2
+    for envDistIndex in range(num_distributions):
+        envDistribution = allEnvDistributions[envDistIndex]
+        # TODO this is probably not that useful
+        """
+        for env in allEnvs:
+            if env not in envDistribution.keys():
+                envDistribution[env] = 0
+        print(envDistribution)
+        """
+        # Create Mapping from the string MiniGrid-DoorKey-6x6-
+        # to the actual numbers. Results in tuples of ('6x6', 6) and so on for sorting later
+        shortenedEnvFreqMapping = {numKey.split('-')[-1]: val for numKey, val in envDistribution.items()}
+        levelSizeAsNumbers: list[int] = sorted([int(fullKey.split("x")[0]) for fullKey in shortenedEnvFreqMapping])
         numStrKeyMapping = []
-        for numKey in keyValues:
-            for dictKey in numericStrDict:
+        for numKey in levelSizeAsNumbers:
+            for dictKey in shortenedEnvFreqMapping:
                 if str(numKey) == dictKey.split('x')[0]:
                     numStrKeyMapping.append((dictKey, numKey))
                     break
 
         sortedKeys = [fullKey for fullKey, _ in numStrKeyMapping]
-        finalDict = {k: numericStrDict[k] for k in sortedKeys}
+        finalDict = {k: shortenedEnvFreqMapping[k] for k in sortedKeys}
 
         envs = list(finalDict.keys())
         envOccurrences = list(finalDict.values())
-        bar_container = ax.bar(envs, envOccurrences, label=modelNamesList[0])
+
+        x = np.arange(len(envs))
+        x = [xi + x_offset + envDistIndex * bar_width for xi in x]
+
+        bar_container = ax.bar(x, envOccurrences, width=bar_width, label=modelNamesList[envDistIndex])
+        # ax.bar_label(bar_container, labels=envOccurrences, fontsize=12, padding=5)
 
     ax.set_ylabel('Occurrence')
     ax.set_title(titleInfo)
-    ax.set_ylim(0, 30)
-
-    # Add labels to the bars
-    ax.bar_label(bar_container, labels=envOccurrences, fontsize=12, padding=5)
-    ax.legend()
+    ax.set_xticks(np.arange(len(envs)))
+    ax.set_xticklabels(envs)
+    ax.legend(loc='upper right', bbox_to_anchor=(1.2, 1))  # move legend outside of plot
+    ax.yaxis.set_major_locator(MaxNLocator(integer=True))
     plt.show()
-
-    # TODO find way to plot multiple models at once (and show some relevant legend for info of model name or sth like that)
-    # TODO extra args option to only load 1 model
-    # TODO experiment comparison: long iterPerStep with many Gen, with low iterPerStep with low gen
-
-    # TODO plot the snapshot vs curricReward problem ---> do some experiments. How does the curricLength influence results? How does gamma influence results?
-
-    # TODO: low prio stuff
-    # find out a way to properly plot the difficulty list / maybe how it influences the results; and maybe how you can improve it so that it is not even needed in the first place
-    # save the plots
 
 
 if __name__ == "__main__":
@@ -154,6 +165,7 @@ if __name__ == "__main__":
     for logFilePath in logFilePaths:
         if "16x16" in logFilePath:
             continue
+        # TODO maybe create 2 plots showing each distribution once (12x12 vs 16x16 variant ?)
         if os.path.exists(logFilePath):
             with open(logFilePath, 'r') as f:
                 trainingInfoDictionary = json.loads(f.read())
@@ -166,12 +178,23 @@ if __name__ == "__main__":
 
     modelNames = [res.modelName for res in resultClasses]
 
-    plotSnapshotEnvDistribution(resultClasses, "Distribution of envs of best performing curricula", modelNames)
-    # plotEnvsUsedDistribution(resultClasses, "all Curric Distribution", modelNames)
-    # plotEnvsUsedDistribution(resultClasses, "snapshot Distribution", modelNames)
+    # plotSnapshotEnvDistribution(resultClasses, "Distribution of envs of best performing curricula", modelNames)
+    plotDistributionOfBestCurric(resultClasses, "Distribution of env occurence from best performing curricula", modelNames)
     # TODO are there other distributions that are useful?
 
     #plotSnapshotPerformance(resultClasses, "Snapshot Performance", modelNames)
     #plotBestCurriculumResults(resultClasses, "Best Curriculum Results", modelNames)
     # plotEpochAvgCurricReward(resultClasses, "Average Curriculum Reward of all Generations in an epoch", modelNames) # TODO this should not have a shared x-axis; or at least still use epochs and not scale
+
+
+
+    # TODO find way to plot multiple models at once (and show some relevant legend for info of model name or sth like that)
+    # TODO extra args option to only load 1 model
+    # TODO experiment comparison: long iterPerStep with many Gen, with low iterPerStep with low gen
+
+    # TODO plot the snapshot vs curricReward problem ---> do some experiments. How does the curricLength influence results? How does gamma influence results?
+
+    # TODO: low prio stuff
+    # find out a way to properly plot the difficulty list / maybe how it influences the results; and maybe how you can improve it so that it is not even needed in the first place
+    # save the plots
 
