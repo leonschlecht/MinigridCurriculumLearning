8308922339dd4151b086a2612d5c8d4df4abc1e0
leonschlecht <73598045+leonschlecht@users.noreply.github.com>
2023-06-16 21:43:22 +0200

    temporary logs of next run

--- cl_25_3_3_3.sh
+++ cl_25_3_3_3.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #SBATCH --job-name=c_25_3_3_3
-#SBATCH --output=c_25_3_3_3curricRun_%j_out.txt
+#SBATCH --output=c_6_25_3_3_3curricRun_%j_out.txt
 #SBATCH --time=20:59:00
 #SBATCH --partition=cpu_normal_stud,cpu_long_stud,cadpool_stud
 #SBATCH --exclude=cp2019-11,cc1l01
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=3
 #SBATCH --mem=20G
 #SBATCH --verbose
 
 echo "Start2"
 
-srun -c 2 -v python3 -m scripts.trainCurriculum --procs 48 --numCurric 3 --stepsPerCurric 3 --nGen 3 --iterPerEnv 25000 --model s9152_25k_3step_3gen_3curric --trainEpochs 200 --seed 9152
+srun -c 3 -v python3 -m scripts.trainCurriculum --procs 48 --numCurric 3 --stepsPerCurric 3 --nGen 3 --iterPerEnv 25000 --model 25k_3step_3gen_3curric_s2529 --trainEpochs 200 --seed 2529
 
 echo "END run"
--- rrh.sh
+++ rrh.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-#SBATCH --job-name=rrh_c
-#SBATCH --output=rrh_cRun_%j_out.txt
-#SBATCH --time=20:59:00
+#SBATCH --job-name=rrh_c3
+#SBATCH --output=rrh10_cRun_%j_out.txt
+#SBATCH --time=21:59:00
 #SBATCH --partition=cpu_normal_stud,cpu_long_stud,cadpool_stud
 #SBATCH --exclude=cp2019-11,cc1l01
-#SBATCH --cpus-per-task=6
+#SBATCH --cpus-per-task=3
 #SBATCH --mem=20G
 #SBATCH --verbose
 
 echo "Start2"
 
-srun -c 3 -v python3 -m scripts.trainCurriculum --procs 48 --iterPerEnv 150000 --model s_1_rrh --trainEpochs 37 --trainRandomRH
-srun -c 3 -v python3 -m scripts.trainCurriculum --procs 48 --iterPerEnv 150000 --model s8515_rrh --trainEpochs 37 --seed 8515 --trainRandomRH
+srun -c 3 -v python3 -m scripts.trainCurriculum --procs 48 --iterPerEnv 150000 --model rrh_3053 --trainEpochs 37 --seed 3053 --trainRandomRH
 
 
 echo "END run"
