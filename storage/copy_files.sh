14e65b74b719e5873badeab9fecd07a45f6384ec
leonschlecht <73598045+leonschlecht@users.noreply.github.com>
2023-06-20 16:26:33 +0200

    Grorup Experiments together

--- storage/copy_files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-while read -r filename; do
-    dirname=$(dirname "$filename")
-    echo "$filename"
-    cp "$filename" "~/Test12/MinigridCurriculumLearning/logs/$dirname/status.json"
-done < filelist.txt
-
