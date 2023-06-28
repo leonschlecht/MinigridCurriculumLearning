bbf37a6352efa602dc830c079c4022e6b27f13e7
leonschlecht <73598045+leonschlecht@users.noreply.github.com>
2023-06-28 13:24:55 +0200

    txt logs

--- cl100_3_3_3.sh
+++ cl100_3_3_3.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=c100_3_3_3
-#SBATCH --output=c100_3_3_3curricRun_%j_out.txt
-#SBATCH --time=23:59:00
-#SBATCH --partition=cpu_normal_stud,cpu_long_stud,cadpool_stud
+#SBATCH --output=GAc100_3_3_3curricRun_%j_out.txt
+#SBATCH --time=47:59:00
+#SBATCH --partition=cpu_long_stud
 #SBATCH --exclude=cp2019-11,cc1l01
-#SBATCH --cpus-per-task=3
-#SBATCH --mem=20G
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=30G
 #SBATCH --verbose
-
 echo "------------Cluster Job Start-----------------------"
-
-srun -c 3 -v python3 -m scripts.trainCurriculum --procs 48 --numCurric 3 --stepsPerCurric 3 --nGen 3 --iterPerEnv 100000 --model NSGA_100k_3step_3gen_3curric --seed 8515
-
+srun -c 6 -v python3 -m scripts.trainCurriculum --procs 32 --numCurric 3 --stepsPerCurric 3 --nGen 3 --iterPerEnv 100000 --model 100k_3step_3gen_3curric --seed 9152
 echo "---------- Cluster Job End ---------------------"
