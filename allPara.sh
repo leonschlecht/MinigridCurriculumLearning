c71ac6db97ee76475d609021e25352d15c5379c3
leonschlecht <73598045+leonschlecht@users.noreply.github.com>
2023-06-30 16:47:18 +0200

    new logs

--- allPara.sh
+++ allPara.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --job-name=allPara2
 #SBATCH --output=allPara2_%j.txt
-#SBATCH --time=11:59:00
-#SBATCH --partition=cpu_normal_stud,cpu_long_stud,cadpool_stud
+#SBATCH --time=23:59:00
+#SBATCH --partition=cpu_normal_stud,cpu_long_stud
 #SBATCH --exclude=cp2019-11,cc1l01
-#SBATCH --cpus-per-task=9
-#SBATCH --mem=24G
+#SBATCH --cpus-per-task=3
+#SBATCH --mem=48G
 #SBATCH --verbose
 echo "Start2"
-srun -c 3 -v python3 -m scripts.trainCurriculum --procs 32 --trainAllParalell --seed 1 --model AllPara
-srun -c 3 -v python3 -m scripts.trainCurriculum --procs 32 --trainAllParalell --seed 1214 --model AllPara
-srun -c 3 -v python3 -m scripts.trainCurriculum --procs 32 --trainAllParalell --seed 2330 --model AllPara
+srun -c 1 -v python3 -m scripts.trainCurriculum --procs 32 --trainAllParalell --seed 185 --model AllPara
+srun -c 1 -v python3 -m scripts.trainCurriculum --procs 32 --trainAllParalell --seed 3053 --model AllPara
+srun -c 1 -v python3 -m scripts.trainCurriculum --procs 32 --trainAllParalell --seed 9030 --model AllPara
 echo "END run"
--- rrh.sh
+++ rrh.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=rrh_c4
-#SBATCH --output=r22rh13_cRun_%j_ou2t2.txt
+#SBATCH --output=250kRRHcRun_%j_ou2t2.txt
 #SBATCH --time=23:59:00
 #SBATCH --partition=cpu_normal_stud,cpu_long_stud
 #SBATCH --exclude=cp2019-11,cc1l01
-#SBATCH --cpus-per-task=3
-#SBATCH --mem=10G
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=20G
 #SBATCH --verbose
-
 echo "----------Starting RRH--------"
-
-srun -c 3 -v python3 -m scripts.trainCurriculum --procs 48 --iterPerEnv 50000 --model rrh_50k_5step --stepsPerCurric 5 --trainEpochs 200 --trainRandomRH --seed 3053
-
-
+srun -c 1 -v python3 -m scripts.trainCurriculum --procs 32 --iterPerEnv 250000 --model rrh_250k_3step --stepsPerCurric 3 --trainRandomRH --seed 9152
 echo "END run"
