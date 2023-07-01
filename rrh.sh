f0f6d39a0015854764b5ed9b98c1235cc4f10e22
leonschlecht <73598045+leonschlecht@users.noreply.github.com>
2023-07-01 12:18:01 +0200

    new logs

--- rrh.sh
+++ rrh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=rrh_c4
-#SBATCH --output=250kRRHcRun_%j_ou2t2.txt
+#SBATCH --job-name=50krrh_c4
+#SBATCH --output=50k_5sRRHcRun_%j_ou2t2.txt
 #SBATCH --time=23:59:00
 #SBATCH --partition=cpu_normal_stud,cpu_long_stud
 #SBATCH --exclude=cp2019-11,cc1l01
@@ -8,5 +8,5 @@
 #SBATCH --mem=20G
 #SBATCH --verbose
 echo "----------Starting RRH--------"
-srun -c 1 -v python3 -m scripts.trainCurriculum --procs 32 --iterPerEnv 250000 --model rrh_250k_3step --stepsPerCurric 3 --trainRandomRH --seed 9152
+srun -c 1 -v python3 -m scripts.trainCurriculum --procs 32 --iterPerEnv 50000 --model rrh_50k_5step --stepsPerCurric 5 --trainRandomRH --seed 9152
 echo "END run"
