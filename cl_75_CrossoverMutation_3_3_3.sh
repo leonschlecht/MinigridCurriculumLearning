f6ead220cab0d3f5f103786ec51633fb8f438c52
leonschlecht <73598045+leonschlecht@users.noreply.github.com>
2023-07-29 11:17:59 +0200

    new runs

--- cl_75_CrossoverMutation_3_3_3.sh
+++ cl_75_CrossoverMutation_3_3_3.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=CrMu5050
-#SBATCH --output=GA_75_333CrMu5050_%j_out.txt
-#SBATCH --time=47:59:00
+#SBATCH --job-name=CrMu8080
+#SBATCH --output=GA_75_333CrMu8080_%j_out.txt
+#SBATCH --time=71:59:00
 #SBATCH --partition=cpu_long_stud
 #SBATCH --exclude=cp2019-11,cc1l01
 #SBATCH --cpus-per-task=2
-#SBATCH --mem=18G
+#SBATCH --mem=24G
 #SBATCH --verbose
 echo "------------Cluster Job Start-----------------------"
-srun -c 2 -v python3 -m scripts.trainCurriculum --procs 24 --numCurric 3 --stepsPerCurric 3 --nGen 3 --iterPerEnv 75000 --model GA_75k_3step_3gen_3curric --mutationProb 0.5 --crossoverProb 0.5 --seed 1
+srun -c 2 -v python3 -m scripts.trainCurriculum --procs 24 --numCurric 3 --stepsPerCurric 3 --nGen 3 --iterPerEnv 75000 --model GA_75k_3step_3gen_3curric_Cr80_Mu80 --mutationProb 0.8 --crossoverProb 0.8 --seed 8515
 echo "---------- Cluster Job End ---------------------"
--- rrh.sh
+++ rrh.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=rrhC6
-#SBATCH --output=rrh_100_%j_out.txt
-#SBATCH --time=23:59:00
-#SBATCH --partition=cpu_normal_stud
+#SBATCH --job-name=rrh250
+#SBATCH --output=rrh_250_%j_out.txt
+#SBATCH --time=47:59:00
+#SBATCH --partition=cpu_long_stud
 #SBATCH --exclude=cp2019-11,cc1l01
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=16G
 #SBATCH --verbose
 echo "------------Cluster Job Start-----------------------"
-srun -c 2 -v python3 -m scripts.trainCurriculum --noRewardShaping --procs 24 --iterPerEnv 100000 --model rrh_100k_3step_6curric --stepsPerCurric 3 --numCurric 6 --trainRandomRH --seed 8515
-
+srun -c 2 -v python3 -m scripts.trainCurriculum --noRewardShaping --procs 24 --iterPerEnv 250000 --model rrh_250k_3step_3curric --stepsPerCurric 3 --numCurric 3 --trainRandomRH --seed 3053
 echo "---------- Cluster Job End ---------------------"
