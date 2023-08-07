1517c2de2018e9d86f7754d7526769a210853181
leonschlecht <73598045+leonschlecht@users.noreply.github.com>
2023-08-07 17:15:32 +0200

    ppo only test runs

--- allPara.sh
+++ allPara.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=AllParav2
-#SBATCH --output=AllPara_%j_out.txt
-#SBATCH --time=23:59:00
+#SBATCH --job-name=ppo
+#SBATCH --output=ppo_%j_out.txt
+#SBATCH --time=01:59:00
 #SBATCH --partition=cpu_normal_stud
 #SBATCH --exclude=cp2019-11,cc1l01
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=16G
 #SBATCH --verbose
 echo "------------Cluster Job Start-----------------------"
-srun -c 2 -v python3 -m scripts.trainCurriculum --procs 16 --trainAllParalell --asCurric --model AllPara_AsCurric_v2 --noRewardShaping --iterPerEnv 5000 --seed 1
+srun -c 2 -v python3 -m scripts.trainCurriculum  --trainAllParalell  --model ppoAttempt8x8 --iterPerEnv 50000 --procs 16 --ppoEnv 1 --seed 1
 echo "---------- Cluster Job End ---------------------"
 
 
