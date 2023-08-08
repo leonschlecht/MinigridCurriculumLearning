606bcc88048f3b41158f3afd555af12df34e5e08
leonschlecht <73598045+leonschlecht@users.noreply.github.com>
2023-08-08 10:25:53 +0200

    new logs ppo

--- allPara.sh
+++ allPara.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=ppo
-#SBATCH --output=ppo12x12_%j_out.txt
+#SBATCH --job-name=ppo10x10
+#SBATCH --output=ppo10x10_%j_out.txt
 #SBATCH --time=23:59:00
 #SBATCH --partition=cpu_normal_stud
 #SBATCH --exclude=cp2019-11,cc1l01
@@ -8,11 +8,11 @@
 #SBATCH --mem=24G
 #SBATCH --verbose
 echo "------------Cluster Job Start-----------------------"
-srun -c 2 -v python3 -m scripts.trainCurriculum  --trainAllParalell  --model PPO_12x12 --iterPerEnv 50000 --procs 16 --ppoEnv 3 --seed 1
-srun -c 2 -v python3 -m scripts.trainCurriculum  --trainAllParalell  --model PPO_12x12 --iterPerEnv 50000 --procs 16 --ppoEnv 3 --seed 185
-srun -c 2 -v python3 -m scripts.trainCurriculum  --trainAllParalell  --model PPO_12x12 --iterPerEnv 50000 --procs 16 --ppoEnv 3 --seed 1214
-srun -c 2 -v python3 -m scripts.trainCurriculum  --trainAllParalell  --model PPO_12x12 --iterPerEnv 50000 --procs 16 --ppoEnv 3 --seed 2330
-srun -c 2 -v python3 -m scripts.trainCurriculum  --trainAllParalell  --model PPO_12x12 --iterPerEnv 50000 --procs 16 --ppoEnv 3 --seed 2529
+srun -c 2 -v python3 -m scripts.trainCurriculum  --trainAllParalell  --model PPO_10x10 --iterPerEnv 50000 --procs 16 --ppoEnv 2 --seed 3053
+srun -c 2 -v python3 -m scripts.trainCurriculum  --trainAllParalell  --model PPO_10x10 --iterPerEnv 50000 --procs 16 --ppoEnv 2 --seed 8258
+srun -c 2 -v python3 -m scripts.trainCurriculum  --trainAllParalell  --model PPO_10x10 --iterPerEnv 50000 --procs 16 --ppoEnv 2 --seed 9152
+srun -c 2 -v python3 -m scripts.trainCurriculum  --trainAllParalell  --model PPO_10x10 --iterPerEnv 50000 --procs 16 --ppoEnv 2 --seed 8515
+srun -c 2 -v python3 -m scripts.trainCurriculum  --trainAllParalell  --model PPO_10x10 --iterPerEnv 50000 --procs 16 --ppoEnv 2 --seed 1
 echo "---------- Cluster Job End ---------------------"
 
 
