5eb55dec1de4fc7a0c5c05168558021d874387f5
leonschlecht <73598045+leonschlecht@users.noreply.github.com>
2023-08-27 12:01:07 +0200

    new runs

--- cl_dynamicObstacle.sh
+++ cl_dynamicObstacle.sh
@@ -5,10 +5,10 @@
 #SBATCH --partition=cpu_long_stud
 #SBATCH --exclude=cp2019-11,cc1l01
 #SBATCH --cpus-per-task=2
-#SBATCH --mem=8G
+#SBATCH --mem=2G
 #SBATCH --verbose
 echo "------------Cluster Job Start-----------------------"
-srun -c 2 -v python3 trainCurriculum.py --procs 24 --noRewardShaping --stepsPerCurric 3 --nGen 3 --numCurric 6 --iterPerEnv 100000 --model 5_rrh_100k_6curric_3step --dynamicObstacle --trainRandomRH --seed 9152
+srun -c 2 -v python3 trainCurriculum.py --procs 24 --noRewardShaping --stepsPerCurric 3 --nGen 2 --numCurric 4 --iterPerEnv 100000 --model 5_100k_3step_2gen_4curric --dynamicObstacle --seed 2330
 echo "---------- Cluster Job End ---------------------"
 
 
